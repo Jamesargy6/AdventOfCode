7e165f9ce3e9b8cd4be4dbd81f3d7a0d57b65074
James Argyropoulos <jamesargy6@gmail.com>
2017-12-03 22:05:27 -0500

    code improvements: -added commenst -removed whitespace -greatly simplified p2 by realizing that if a%b = 0, b%a != 0

--- 2017/day2/day2p1.py
+++ 2017/day2/day2p1.py
@@ -1,27 +1,23 @@
 import sys, math
 
-
 file = open("day2input.txt", "r")
 checksumLines = file.read().split("\n")
-
 
 result = 0
 for line in checksumLines:
 	numbers = line.split()
 	print("Line ", line)
+	#ensure that the mins and maxes will always initialize with the first number
 	currentMin = math.inf
 	currentMax = -1
 	for number in numbers:
+		#update current min/max
 		currentMin = int(number) if int(number) < currentMin  else currentMin
 		currentMax = int(number) if int(number) > currentMax  else currentMax
 	print("Max: ", currentMax)
 	print("Min: ", currentMin)
+	#update the check sum with max - min
 	result += currentMax - currentMin
 	print("Result: ", result)
 
 print("TOTAL: ", result)
-
-
-
-
-
--- 2017/day2/day2p2.py
+++ 2017/day2/day2p2.py
@@ -1,37 +1,22 @@
 import sys, math
 
-
 file = open("day2input.txt", "r")
 checksumLines = file.read().split("\n")
-
 
 result = 0
 for line in checksumLines:
 	numbers = line.split()
 	print("Line ", line)
-	found = False
+	#check every number with every other in the list
 	for number in numbers:
 		number = int(number)
 		for number2 in numbers:
 			number2 = int(number2)
 			if number != number2:
+
+				#if the numbers have a zero-remainder division, add their result
 				if number % number2 == 0:
-					found = True
 					result += number / number2
 					print("Number: ", number, "Number 2: ", number2)
-					print("Result: ", result)
-				if number2 % number == 0:
-					found = True
-					result += number2 / number
-					print("Number: ", number, "Number 2: ", number2)
-					print("Result: ", result)
-			if found: break
-		if found: break
-
 
 print("TOTAL: ", result)
-
-
-
-
-
