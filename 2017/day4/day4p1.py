a5dd7f3922493cbfd65dd6d76909f5c3339d1804
James Argyropoulos <jamesargy6@gmail.com>
2017-12-04 00:15:49 -0500

    day4 completed in 13 minutes

---
+++ 2017/day4/day4p1.py
@@ -0,0 +1,23 @@
+import sys
+
+file = open("day4input.txt", "r")
+passphraseLines = file.read().split("\n")
+validPhrases = 0
+index = 0
+for line in passphraseLines:
+	isValidLine = True
+	words = []
+	for word in line.split():
+		if word in words:
+			print("Line ", index, " invalid: ", word)
+			isValidLine = False
+			break
+		else:
+			words.append(word)
+	if isValidLine:
+		print("Line ", index, " is valid: ", line)
+		validPhrases += 1
+
+print("number of valid phrases: ", validPhrases)
+
+
