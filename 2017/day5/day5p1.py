f70e7092e75dd4195f51eb90704cfc08e0a0278b
James Argyropoulos <jamesargy6@gmail.com>
2017-12-05 01:03:00 -0500

    code cleanup, pretty happy with the final result, if not the time it took.

--- 2017/day5/day5p1.py
+++ 2017/day5/day5p1.py
@@ -3,23 +3,16 @@ import sys
 file = open("../inputs/day5input.txt", "r")
 
 stepList = file.read().split("\n")
+stepList = list(map(int, stepList))
 
 counter = 0
 currentIndex = 0
-newValue = 0
-print(stepList)
 while currentIndex >= 0 and currentIndex < len(stepList):
 	counter += 1
-	#print("Counter: ", counter)
-
-	#add the current value to the current index. if it is out of the bounds of hte list, return the counter.
 	prevIndex = currentIndex
-	currentIndex += int(stepList[currentIndex])
-	stepList[prevIndex] = int(stepList[prevIndex]) + 1
-	#print("new index: ", currentIndex)
-	#print(stepList)
-
+	#add the current value to the current index. 
+	currentIndex += stepList[currentIndex]
+	#set up the current value for future iterations
+	stepList[prevIndex] = stepList[prevIndex] + 1
 
 print("Counter: ", counter)
-
-
--- 2017/day5/day5p2.py
+++ 2017/day5/day5p2.py
@@ -3,26 +3,20 @@ import sys
 file = open("../inputs/day5input.txt", "r")
 
 stepList = file.read().split("\n")
+stepList = list(map(int, stepList))
 
 counter = 0
 currentIndex = 0
-newValue = 0
-print(stepList)
 while currentIndex >= 0 and currentIndex < len(stepList):
 	counter += 1
-	#print("Counter: ", counter)
-
-	#add the current value to the current index. if it is out of the bounds of hte list, return the counter.
 	prevIndex = currentIndex
-	currentIndex += int(stepList[currentIndex])
-	if int(stepList[prevIndex]) >= 3:
-		stepList[prevIndex] = int(stepList[prevIndex]) - 1
+	#add the current value to the current index. 
+	currentIndex += stepList[currentIndex]
+	#set up the current value for future iterations
+	if stepList[prevIndex] >= 3:
+		stepList[prevIndex] = stepList[prevIndex] - 1
 	else:
-		stepList[prevIndex] = int(stepList[prevIndex]) + 1
-	#print("new index: ", currentIndex)
-	#print(stepList)
-
+		stepList[prevIndex] = stepList[prevIndex] + 1
 
 print("Counter: ", counter)
-print("new index: ", currentIndex)
 
