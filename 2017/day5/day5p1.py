b400551a93b6059783f4d59f7107eb6ea281d795
James Argyropoulos <jamesargy6@gmail.com>
2017-12-05 00:48:18 -0500

    day 5 finished. took way longer than it should have

---
+++ 2017/day5/day5p1.py
@@ -0,0 +1,25 @@
+import sys
+
+file = open("../inputs/day5input.txt", "r")
+
+stepList = file.read().split("\n")
+
+counter = 0
+currentIndex = 0
+newValue = 0
+print(stepList)
+while currentIndex >= 0 and currentIndex < len(stepList):
+	counter += 1
+	#print("Counter: ", counter)
+
+	#add the current value to the current index. if it is out of the bounds of hte list, return the counter.
+	prevIndex = currentIndex
+	currentIndex += int(stepList[currentIndex])
+	stepList[prevIndex] = int(stepList[prevIndex]) + 1
+	#print("new index: ", currentIndex)
+	#print(stepList)
+
+
+print("Counter: ", counter)
+
+
