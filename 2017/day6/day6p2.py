620151571ae2391e9de79fd38bc5ab71751b8297
James Argyropoulos <jamesargy6@gmail.com>
2017-12-06 00:32:15 -0500

    day 6 completed, overall in under 20 minutes and in 2nd in the LBi leaderboard. pretty happy with the result, though I was very close very early and couldn't get past a simple mistake (line 14).

---
+++ 2017/day6/day6p2.py
@@ -0,0 +1,31 @@
+import sys
+
+file = open("../inputs/day6input.txt", "r")
+
+bankArray = file.read().split()
+bankArray = list(map(int, bankArray))
+
+configurations = []
+counter = 0
+configurations.append(bankArray)
+#print("configurations: ", configurations)
+
+while True:
+	newArray = bankArray[:]
+	currentValue = max(newArray)
+	currentIndex = newArray.index(currentValue) # tie won by smallest index
+
+	newArray[currentIndex] = 0
+	while currentValue > 0:
+		currentIndex = (currentIndex + 1)%len(newArray)
+		newArray[currentIndex] += 1
+		currentValue -= 1
+	counter += 1
+	#print("configurations: ", configurations)
+	for config in configurations:
+		if newArray == config:
+			print("Result: ", configurations.index(newArray))
+			exit()
+	configurations.append(newArray)
+	bankArray = newArray
+
