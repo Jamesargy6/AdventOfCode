b8a30511e40375551d6c4c4e7457951469957d3f
James Argyropoulos <jargyropoulos@lbisoftware.com>
2017-12-08 18:26:11 -0500

    day 7 part 1 code cleanup, part 2 starting to re-approach

--- 2017/day7/day7p1.py
+++ 2017/day7/day7p1.py
@@ -7,30 +7,20 @@ instructions = file.read().split("\n")
 #initialize an array of programs, and a list of every parent-child relationship
 childParentRelationships = []
 programList = []
+childList = []
+
 for ins in instructions:
 	parent = ins.split()[0]
-	#if it is not a top level program
+	#if the program has children, note their relationship and add each child to its own list
 	if ins.find("->") != -1:
-
 		rawChildren = ins.split("->")[1]
-		children = rawChildren.split(", ")
+		children = rawChildren.split(",")
+		children = [x.strip(' ') for x in children]
 		print("Parent {0} has the following children: {1}".format(parent, children))
 		childParentRelationships.append({"parent" : parent, "children" : children})
+		childList += children
 	programList.append(parent)
 
-#iterate through relationships. add all unique children to a new set
-children = []
-for rel in childParentRelationships:
-	print(rel["children"])
-	for c in  rel["children"]:
-		print("Child to be added: ", c)
-		children.append(c.strip())
-		print(children)
-
-res = set(programList).difference(set(children))
-
-print(res)
-
-
-
 #the difference between the parent and child sets is the solution
+res = set(programList).difference(set(childList))
+print(res)
