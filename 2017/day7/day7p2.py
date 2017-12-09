fab81ae34494de5336961423fd5ef0db2dfd6925
James Argyropoulos <jamesargy6@gmail.com>
2017-12-09 01:16:43 -0500

    day7p2 completed and commented.

--- 2017/day7/day7p2.py
+++ 2017/day7/day7p2.py
@@ -5,33 +5,59 @@ file = open("../inputs/day7input.txt", "r")
 instructions = file.read().split("\n")
 
 #initialize an array of programs, and a list of every parent-child relationship
-childParentRelationships = []
-programMap = []
+childParentRelationships = {}
+programMap = {}
 programList = []
 childList = []
 
 
 for ins in instructions:
 	parent = ins.split()[0]
+	weight = int(ins[ins.find("(")+1:ins.find(")")])
 	#if the program has children, note their relationship and add each child to its own list
 	if ins.find("->") != -1:
 		rawChildren = ins.split("->")[1]
-		weight = ins[ins.find("(")+1:ins.find(")")]
 		children = rawChildren.split(",")
 		children = [x.strip(' ') for x in children]
-		print("Parent {0} has the following children: {1}".format(parent, children))
-		childParentRelationships.append({"parent" : parent, "children" : children})
+		childParentRelationships[parent] = children
 		programMap[parent] = {"weight" : weight, "cumWeight" : 0}
 		childList += children
 	else:
-		programMap.append({"program" : parent, "weight" : weight, "cumWeight" : weight})
+		programMap[parent] = {"weight" : weight, "cumWeight" : weight}
 	programList.append(parent)
 
 #the difference between the parent and child sets is the solution
-res = next(iter(set(programList).difference(set(childList))))
-print(res)
+root = next(iter(set(programList).difference(set(childList))))
 
 #calculate the cumulative weights of each node
-while programMap[res]["cumWeight"] == 0:
+while programMap[root]["cumWeight"] == 0:
 	for rel in childParentRelationships:
+		runningTotalWeight = programMap[rel]["weight"]
+		childrenComplete = True
+		for child in childParentRelationships[rel]:
+			if programMap[child]["cumWeight"] > 0:
+				runningTotalWeight += programMap[child]["cumWeight"]
+			else:
+				childrenComplete = False
+
+		if childrenComplete:
+			programMap[rel]["cumWeight"] = runningTotalWeight
+
+
+print(programMap)
+
+#for each relationship, is the cumulative weight of each child the same? 
+for rel in childParentRelationships:
+	cumulativeWeights = []
+	programs = []
+	for child in childParentRelationships[rel]:
+		cumulativeWeights.append(programMap[child]["cumWeight"])
+		programs.append(programMap[child])
+	#if not so, get the difference of the odd child's value plus/minus the difference in he cumulative values
+	if len(set(cumulativeWeights)) > 1:
+		print("Bad node Found!: ")
+		for p in programs:
+			print(p)
+
+
 		
