1f8b75881ff359206f788927d522aef464b072b7
James Argyropoulos <jamesargy6@gmail.com>
2017-12-11 01:30:49 -0500

    day10 code cleanup. removed some unneccessary variables, comments and formatting.

--- 2017/day10/day10p1.py
+++ 2017/day10/day10p1.py
@@ -4,26 +4,29 @@ file = open("../inputs/day10input.txt", "r")
 
 lengths = file.read().split(",")
 
+#set up our list
 llist = []
 for x in range(0,256):
 	llist.append(x)
 
 llistLen = len(llist)
-
 currentPos = 0
 skipSize = 0
 
+
 for l in lengths:
 	l = int(l)
+	#tempList holds the initial sublist that needs to be reversed
 	tempList = []
 	for x in range(0,l):
 		tempList.append(llist[(currentPos + x)%llistLen])
+	#reverse it, and "put back" each item back at the appropriate index
 	tempList = tempList[::-1]
 	for x in range(0,l):
 		llist[(currentPos + x)%llistLen] = tempList[x]
 
+	#index logic
 	currentPos += (l + skipSize)%llistLen
 	skipSize += 1
-	print(llist)
 	
 print(llist[0]*llist[1])
