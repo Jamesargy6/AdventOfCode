088f1dce08998e17fd4212b8fbaf3b8703963a35
James Argyropoulos <jamesargy6@gmail.com>
2017-12-11 01:24:49 -0500

    day 11 finished. Did not do this challenge at midnight, but was able to think about my approach for a while. ended up coding both parts pretty quickly (p2 13 mins)

---
+++ 2017/day11/day11p2.py
@@ -0,0 +1,27 @@
+import sys
+from operator import add
+
+file = open("../inputs/day11input.txt", "r")
+
+directions = file.read().split(",")
+
+#we will use this to calculate our current coordinate afeter each iteration
+directList = {'ne' : [1,0,-1],
+				'se' : [1,-1,0],
+				's' : [0,-1,1],
+				'sw' : [-1,0,1],
+				'nw' : [-1,1,0],
+				'n' : [0,1,-1]
+		}
+
+currentCoord = [0,0,0]
+
+maxDist = 0
+#calculate the coordinate for each direction and print the result
+for d in directions:
+	currentCoord = [a + b for a, b in zip(currentCoord, directList[d])]
+	maxDist = max(max(abs(x) for x in currentCoord), maxDist)
+	print(currentCoord)
+
+	
+print(maxDist)
