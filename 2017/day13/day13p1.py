f515f80b25b3ba8d5e14cd8d102aaa39ae214e4f
James Argyropoulos <jargyropoulos@lbisoftware.com>
2017-12-13 13:40:51 -0500

    day13 code cleanup. part 1 just got some comments, but part 2 got a major overhaul, cutting the time to run to seconds (from about 45 minutes) by replacing array iteration with a clever modulo check

--- 2017/day13/day13p1.py
+++ 2017/day13/day13p1.py
@@ -7,44 +7,39 @@ directions = file.read().split("\n")
 layers = {}
 incrementing = {}
 totalLayers = 0
+
+#map each layer with an initialized array at that location, set with the scanner in the first position
 for d in directions:
 	dSplit = d.split(":")
-
 	leftSide = int(dSplit[0])
 	rightSide = int(dSplit[1].strip())
-
-	print("Left side: {0}, Right Side: {1}".format(leftSide, rightSide))
 
 	layers[leftSide] = [0]*rightSide
 	layers[leftSide][0] = 1
 	totalLayers = leftSide
 
 	incrementing[leftSide] = True
-print(layers)
 
 result = 0
+#for every "tick", move the pointer. if the scanner exists at that location, multiply the layer's location by the length of the array at that layer
 for x in range(0,totalLayers+1):
 	if x in layers:
 		if layers[x][0] == 1:
-			print("multiplying {0} and {1}".format(x, len(layers[x])))
 			result += x * len(layers[x])
 
-
+	#move the scanner in each layer
 	for y in layers:
 		currentIndex = layers[y].index(1)
 		if currentIndex == len(layers[y])-1:
 			incrementing[y] = False
 		elif currentIndex == 0:
 			incrementing[y] = True
-		print(layers[y])
 		layers[y][currentIndex] = 0
 		if incrementing[y]:
 			currentIndex += 1
 		else:
 			currentIndex -= 1
-		print("New index for layer {0}: {1}".format(y, currentIndex))
 		layers[y][currentIndex] = 1
-		print(layers[y])
 
 print(result)
 
--- 2017/day13/day13p2.py
+++ 2017/day13/day13p2.py
@@ -6,76 +6,27 @@ file = open("../inputs/day13input.txt", "r")
 directions = file.read().split("\n")
 
 layers = {}
-incrementing = {}
-totalLayers = 0
+
+#map each location to the length of the array at that location
 for d in directions:
 	dSplit = d.split(":")
-
 	leftSide = int(dSplit[0])
 	rightSide = int(dSplit[1].strip())
-
-	print("Left side: {0}, Right Side: {1}".format(leftSide, rightSide))
-
-	layers[leftSide] = [0]*rightSide
-	layers[leftSide][0] = 1
-	totalLayers = leftSide
-
-	incrementing[leftSide] = True
-print(layers)
+	layers[leftSide] = rightSide
 
 delay = 0
 cont = True
 
-def iterateScanner():
-	for y in layers:
-		currentIndex = layers[y].index(1)
-		if currentIndex == len(layers[y])-1:
-			incrementing[y] = False
-		elif currentIndex == 0:
-			incrementing[y] = True
-		layers[y][currentIndex] = 0
-		if incrementing[y]:
-			currentIndex += 1
-		else:
-			currentIndex -= 1
-		layers[y][currentIndex] = 1
-
-
-def iterateSingleScanner(y):
-	currentIndex = layers[y].index(1)
-	if currentIndex == len(layers[y])-1:
-		incrementing[y] = False
-	elif currentIndex == 0:
-		incrementing[y] = True
-	layers[y][currentIndex] = 0
-	if incrementing[y]:
-		currentIndex += 1
-	else:
-		currentIndex -= 1
-	layers[y][currentIndex] = 1
-
-
-masterLayers = copy.deepcopy(layers)
-print(masterLayers)
+#iterate until we find the correct delay
 while cont:
-	if delay%1000 == 0:
-		print(delay)	
-	for y in layers:
-		for x in range(0, delay%(2*len(layers[y])-2)):
-			iterateSingleScanner(y)
-
 	seen = False
-	for x in range(0,totalLayers+1):
+	#for each layer, see if the scanner will be at index 0 after delay + y steps, where y is the location of the layer
+	for y in layers:
 		if not seen:
-			if x in layers:
-				if layers[x][0] == 1:
-						delay += 1
-						seen = True
-			iterateScanner()
+			if (delay + y)%(2*layers[y]-2) == 0:
+				delay += 1
+				seen = True
 	cont = seen
-	layers = copy.deepcopy(masterLayers)
-
-	
 
 print(delay)
 
