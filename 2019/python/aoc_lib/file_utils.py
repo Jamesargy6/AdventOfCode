9b48b4b71a2730739740382dd9a3fa1ef5748e2c
James <jamesargy6@gmail.com>
2019-12-02 22:27:48 -0800

    day 3 complete

--- 2019/python/aoc_lib/file_utils.py
+++ 2019/python/aoc_lib/file_utils.py
@@ -11,4 +11,7 @@ def read_file_into_int_list(filename: str) -> List[int]:
 	return [int(line.rstrip('\n')) for line in open(filename)]
 
 def read_line_into_int_list(filename: str) -> List[int]:
-	return list(map(int, open(filename).read().split(','))
+	return list(map(int, open(filename).read().split(',')))
+
+def read_line_into_str_list(filename: str) -> List[str]:
+	return open(filename).read().split(',')
---
+++ 2019/python/day3.py
@@ -0,0 +1,82 @@
+from aoc_lib import file_utils
+from dataclasses import dataclass
+from typing import List
+from collections import namedtuple
+
+
+Instruction = namedtuple('Instruction', 'direction distance')
+
+@dataclass
+class Position:
+	x: int
+	y: int
+	distance: int = 0
+
+@dataclass
+class LineSegment:
+	start: Position
+	end: Position
+	distance: int
+
+def get_instruction(raw_ins: str) -> Instruction:
+	return Instruction(raw_ins[0], int(raw_ins[1:]))
+
+
+
+move_dict = {
+	'L': lambda x, y, distance: Position(x-distance, y),
+	'R': lambda x, y, distance: Position(x+distance, y),
+	'U': lambda x, y, distance: Position(x, y+distance),
+	'D': lambda x, y, distance: Position(x, y-distance)
+}
+
+def get_line_segment(ins: Instruction, pos: Position) -> (LineSegment, Position):
+	move = move_dict[ins.direction]
+	next_pos = move(pos.x, pos.y, ins.distance)
+
+	return LineSegment(pos, next_pos, ins.distance), next_pos
+
+def generate_wire(instructions: List[Instruction]) -> List[LineSegment]:
+	pos = Position(0,0)
+	wire: List[LineSegment] = []
+	for ins in instructions:
+		line_seg, pos = get_line_segment(ins, pos)
+		wire.append(line_seg)
+	return wire
+
+def between(a: int, b: int, x: int) -> bool:
+	return a < x < b or a > x > b
+
+def find_intersections(wire_a: List[LineSegment], wire_b: List[LineSegment]) -> List[Position]:
+	intersections: List[(Position, int)] = []
+
+	wire_a_steps_taken: int = 0
+	for seg_a in wire_a:
+		wire_b_steps_taken: int = 0
+		for seg_b in wire_b:
+			if between(seg_a.start.x, seg_a.end.x, seg_b.start.x) and between(seg_b.start.y, seg_b.end.y, seg_a.start.y):
+				total_steps_taken = wire_a_steps_taken+wire_b_steps_taken + abs(seg_b.start.x - seg_a.start.x) + abs(seg_b.start.y - seg_a.start.y)
+				intersections.append(Position(seg_b.start.x, seg_a.start.y, total_steps_taken))
+			elif between(seg_b.start.x, seg_b.end.x, seg_a.start.x) and between(seg_a.start.y, seg_a.end.y, seg_b.start.y):
+				total_steps_taken = wire_a_steps_taken+wire_b_steps_taken + abs(seg_b.start.x - seg_a.start.x) + abs(seg_b.start.y - seg_a.start.y)
+				intersections.append(Position(seg_a.start.x, seg_b.start.y, total_steps_taken))
+			wire_b_steps_taken += seg_b.distance
+		wire_a_steps_taken += seg_a.distance
+	return intersections
+
+
+
+if __name__ == '__main__':
+	raw_wires = file_utils.read_file_into_str_list('inputs/day3input.txt')
+	raw_wire_a, raw_wire_b = [raw_wire.split(',') for raw_wire in raw_wires]
+	instructions_a = [get_instruction(raw_ins) for raw_ins in raw_wire_a]
+	instructions_b = [get_instruction(raw_ins) for raw_ins in raw_wire_b]
+	
+	wire_a = generate_wire(instructions_a)
+	wire_b = generate_wire(instructions_b)
+
+	intersections = find_intersections(wire_a, wire_b)
+	min_distance = min(map(lambda pos: abs(pos.x)+abs(pos.y), intersections))
+	print(f'Part 1: {min_distance}')
+	min_steps = min(map(lambda pos: pos.distance, intersections))
+	print(f'Part 2: {min_steps}')
