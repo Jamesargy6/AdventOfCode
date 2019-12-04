7e8cc523f3ef3fa200bcdefa79ec223551cbc278
James <jamesargy6@gmail.com>
2019-12-03 19:37:54 -0800

    cleanup day3

--- 2019/python/day3.py
+++ 2019/python/day3.py
@@ -1,26 +1,16 @@
 from aoc_lib import file_utils
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 from collections import namedtuple
 
 
 Instruction = namedtuple('Instruction', 'direction distance')
 
-@dataclass
-class Position:
-	x: int
-	y: int
-	distance: int = 0
+Position = namedtuple('Position', 'x y')
 
-@dataclass
-class LineSegment:
-	start: Position
-	end: Position
-	distance: int
+LineSegment = namedtuple('LineSegment', 'start end distance')
 
-def get_instruction(raw_ins: str) -> Instruction:
-	return Instruction(raw_ins[0], int(raw_ins[1:]))
-
+Intersection = namedtuple('Intersection', 'position steps_taken')
 
 
 move_dict = {
@@ -30,53 +20,56 @@ move_dict = {
 	'D': lambda x, y, distance: Position(x, y-distance)
 }
 
-def get_line_segment(ins: Instruction, pos: Position) -> (LineSegment, Position):
-	move = move_dict[ins.direction]
-	next_pos = move(pos.x, pos.y, ins.distance)
 
-	return LineSegment(pos, next_pos, ins.distance), next_pos
+def get_instruction(raw_ins: str) -> Instruction:
+	return Instruction(raw_ins[0], int(raw_ins[1:]))
 
 def generate_wire(instructions: List[Instruction]) -> List[LineSegment]:
 	pos = Position(0,0)
 	wire: List[LineSegment] = []
 	for ins in instructions:
-		line_seg, pos = get_line_segment(ins, pos)
-		wire.append(line_seg)
+		move = move_dict[ins.direction]
+		next_pos = move(pos.x, pos.y, ins.distance)
+		wire.append(LineSegment(pos, next_pos, ins.distance))
+		pos = next_pos
 	return wire
 
-def between(a: int, b: int, x: int) -> bool:
-	return a < x < b or a > x > b
-
-def find_intersections(wire_a: List[LineSegment], wire_b: List[LineSegment]) -> List[Position]:
-	intersections: List[(Position, int)] = []
+def find_intersections(wire_a: List[LineSegment], wire_b: List[LineSegment]) -> List[Intersection]:
+	intersections: List[Intersection] = []
 
 	wire_a_steps_taken: int = 0
 	for seg_a in wire_a:
 		wire_b_steps_taken: int = 0
 		for seg_b in wire_b:
-			if between(seg_a.start.x, seg_a.end.x, seg_b.start.x) and between(seg_b.start.y, seg_b.end.y, seg_a.start.y):
+			intersection_position = get_intersection_position(seg_a, seg_b) or get_intersection_position(seg_b, seg_a)
+			if intersection_position:
 				total_steps_taken = wire_a_steps_taken+wire_b_steps_taken + abs(seg_b.start.x - seg_a.start.x) + abs(seg_b.start.y - seg_a.start.y)
-				intersections.append(Position(seg_b.start.x, seg_a.start.y, total_steps_taken))
-			elif between(seg_b.start.x, seg_b.end.x, seg_a.start.x) and between(seg_a.start.y, seg_a.end.y, seg_b.start.y):
-				total_steps_taken = wire_a_steps_taken+wire_b_steps_taken + abs(seg_b.start.x - seg_a.start.x) + abs(seg_b.start.y - seg_a.start.y)
-				intersections.append(Position(seg_a.start.x, seg_b.start.y, total_steps_taken))
+				intersections.append(Intersection(intersection_position, total_steps_taken))
 			wire_b_steps_taken += seg_b.distance
 		wire_a_steps_taken += seg_a.distance
 	return intersections
 
+def get_intersection_position(seg_a: LineSegment, seg_b: LineSegment) -> Position:
+	if between(seg_a.start.x, seg_a.end.x, seg_b.start.x) and between(seg_b.start.y, seg_b.end.y, seg_a.start.y):
+		return Position(seg_b.start.x, seg_a.start.y)
 
+
+def between(a: int, b: int, x: int) -> bool:
+	return a <= x <= b or a >= x >= b
 
 if __name__ == '__main__':
 	raw_wires = file_utils.read_file_into_str_list('inputs/day3input.txt')
 	raw_wire_a, raw_wire_b = [raw_wire.split(',') for raw_wire in raw_wires]
-	instructions_a = [get_instruction(raw_ins) for raw_ins in raw_wire_a]
-	instructions_b = [get_instruction(raw_ins) for raw_ins in raw_wire_b]
+	instructions_a = list(map(get_instruction, raw_wire_a))
+	instructions_b = list(map(get_instruction, raw_wire_b))
 	
 	wire_a = generate_wire(instructions_a)
 	wire_b = generate_wire(instructions_b)
 
 	intersections = find_intersections(wire_a, wire_b)
-	min_distance = min(map(lambda pos: abs(pos.x)+abs(pos.y), intersections))
+	intersections.remove(Intersection(Position(0,0),0))
+	
+	min_distance = min(map(lambda intersection: abs(intersection.position.x)+abs(intersection.position.y), intersections))
 	print(f'Part 1: {min_distance}')
-	min_steps = min(map(lambda pos: pos.distance, intersections))
+	min_steps = min(map(lambda intersection: intersection.steps_taken, intersections))
 	print(f'Part 2: {min_steps}')
