af639075e337804f29f44e4a875c34abf0640f8b
jamesargy6 <jamesargy6@gmail.com>
2019-12-09 16:06:21 -0800

    small intcode improvements

--- 2019/python/aoc_lib/intcode.py
+++ 2019/python/aoc_lib/intcode.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Tuple
+from typing import List, Callable, Tuple, Dict
 from dataclasses import dataclass
 from enum import Enum, auto
 
@@ -6,13 +6,15 @@ from enum import Enum, auto
 class HaltException(Exception):
 	pass
 
+
 class InputException(Exception):
 	pass
+
 
 class ParameterType(Enum): 
 	READ = auto()
 	WRITE = auto()
-	JUMP = auto()
+
 
 @dataclass
 class Instruction:
@@ -20,31 +22,32 @@ class Instruction:
 	parameter_types: List[ParameterType]
 	operation: Callable
 
+
 class ProgramState(Enum): 
 	IDLE = auto()
 	RUNNING = auto()
 	INPUT_WAIT = auto()
 	HALTED = auto()
 
+
 class IntcodeProgram:
-	HALT_OPCODE = 99
 	instruction_pointer: int
 	input_pointer: int
 	relative_base: int
-	memory: List[int]
+	memory: Dict[int, int]
 	inputs: List[int]
 	output: List[int]
 	state: ProgramState
 	write: Callable
 
-	def __init__(self, memory: List[int], inputs: List[int] = []):
+	def __init__(self, program: List[int], inputs: List[int] = []):
 		self.OP_CODES = {
 			1: Instruction(1, [ParameterType.READ, ParameterType.READ, ParameterType.WRITE], self._add),
 			2: Instruction(2, [ParameterType.READ, ParameterType.READ, ParameterType.WRITE], self._mul),
 			3: Instruction(3, [ParameterType.WRITE], self._in),
 			4: Instruction(4, [ParameterType.READ], self._out),
-			5: Instruction(5, [ParameterType.READ, ParameterType.JUMP], self._jit),
-			6: Instruction(6, [ParameterType.READ, ParameterType.JUMP], self._jif),
+			5: Instruction(5, [ParameterType.READ, ParameterType.READ], self._jit),
+			6: Instruction(6, [ParameterType.READ, ParameterType.READ], self._jif),
 			7: Instruction(7, [ParameterType.READ, ParameterType.READ, ParameterType.WRITE], self._lt),
 			8: Instruction(8, [ParameterType.READ, ParameterType.READ, ParameterType.WRITE], self._eq),
 			9: Instruction(9, [ParameterType.READ], self._rel),
@@ -53,7 +56,7 @@ class IntcodeProgram:
 		self.PARAM_MODES = {
 			0: lambda i: self._read_from_memory(i),
 			1: lambda i: i,
-			2: lambda i:self._read_from_memory(i+self.relative_base)
+			2: lambda i: self._read_from_memory(i+self.relative_base)
 		}
 
 		self.WRITE_MODES = {
@@ -65,7 +68,7 @@ class IntcodeProgram:
 		self.input_pointer = 0
 		self.relative_base = 0
 
-		self.memory = memory
+		self.memory = dict(zip(range(len(program)), program))
 		self.inputs = inputs
 		self.output = []
 		self.state = ProgramState.IDLE
@@ -76,10 +79,11 @@ class IntcodeProgram:
 			instruction_code = self.memory[self.instruction_pointer]
 			instruction, parameters = self.prepare_instruction(instruction_code)
 			try:
-				result = instruction.operation(*parameters)
-			except HaltException as e:
+				self.instruction_pointer += 1 + len(parameters)
+				instruction.operation(*parameters)
+			except HaltException:
 				self.state = ProgramState.HALTED
-			except InputException as e:
+			except InputException:
 				self.state = ProgramState.INPUT_WAIT
 
 	def prepare_instruction(self, ins_code: int) -> Tuple[int, List[int]]:
@@ -102,69 +106,50 @@ class IntcodeProgram:
 				parameters.append(self.PARAM_MODES[pm](self.memory[param_index]))
 		return (*parameters, )
 
-
 	def _add(self, ia: int, ib: int, ipos: int):
 		self.write(ia + ib, ipos)
-		self.instruction_pointer += 4
 
 	def _mul(self, ia: int, ib: int, ipos: int):
 		self.write(ia * ib, ipos)
-		self.instruction_pointer += 4
 
 	def _in(self, i: int):
 		if len(self.inputs) > self.input_pointer:
 			self.write(self.inputs[self.input_pointer], i)
 			self.input_pointer += 1
-			self.instruction_pointer += 2
 		else:
 			raise InputException()
 
 	def _out(self, out: int):
 		self.output.append(out)
-		self.instruction_pointer += 2
 
 	def _jit(self, ia: int, j: int):
 		if ia > 0:
 			self.instruction_pointer = j
-			return
-		self.instruction_pointer += 3
 
 	def _jif(self, ia: int, j: int):
 		if ia == 0:
 			self.instruction_pointer = j
-			return
-		self.instruction_pointer += 3
 
 	def _lt(self, ia: int, ib: int, ipos: int):
 		value = 1 if ia < ib else 0
 		self.write(value, ipos)
-		self.instruction_pointer += 4
 
 	def _eq(self, ia: int, ib: int, ipos: int):
 		value = 1 if ia == ib else 0
 		self.write(value, ipos)
-		self.instruction_pointer += 4
 
 	def _rel(self, ia: int):
 		self.relative_base += ia
-		self.instruction_pointer += 2
 
 	def _halt(self):
 		raise HaltException()
 
 	def _write_to_memory(self, value: int, index: int):
-		self._grow_memory(index)
 		self.memory[index] = value
 
 	def _write_to_relative_memory(self, value: int, index: int):
 		rel_index = index+self.relative_base
-		self._grow_memory(rel_index)
 		self.memory[rel_index] = value
 
 	def _read_from_memory(self, index: int):
-		self._grow_memory(index)
 		return self.memory[index]
-
-	def _grow_memory(self, index: int):
-		if index >= len(self.memory):
-			self.memory = self.memory + [0 for _ in range(index-len(self.memory)+1)]
